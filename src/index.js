56090c06a38bc4663b2e579bbde5cd0a22363ee4
Nethmi Ilangamge <nethu0905@gmail.com>
2023-07-04 17:12:12 +1000

    Adding Files

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+reportWebVitals();
